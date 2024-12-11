77aeca8fcbd49bed67448293e0b325c432bd463a
Nicolas <nicolasgfalabella@gmail.com>
2024-12-10 22:36:26 -0300

    Clase 120

--- clase120/main.go
+++ clase120/main.go
@@ -1,10 +1,38 @@
 package main
 
-import(
-	f "fmt"
+import (
 	"encoding/json"
+	_ "encoding/json"
+	f "fmt"
 )
 
+type Persona struct{
+	Nombre string
+	Apellido string
+	Edad int
+}
+
 func main () {
-	
+	s := `[{"Nombre":"Nicolas","Apellido":"Gonzales","Edad":28},{"Nombre":"Adrian","Apellido":"Nario","Edad":48}]`
+	bs := []byte(s)
+
+	f.Printf("%T \n" ,  s)
+	f.Printf("%T \n" ,  bs)
+
+	var personas []Persona
+
+	err := json.Unmarshal( bs , &personas)
+
+	if err != nil {
+		f.Println(err)
+	}
+
+	f.Println("Toda la data : " , personas)
+
+	for i , v := range personas{
+		f.Println("Indice " , i  , " Usuario " , v.Nombre)
+	}
+
+
+
 }
